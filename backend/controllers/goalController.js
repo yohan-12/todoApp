cdf84c3db8c6eb446577d11e51142e25a831e337
Yohan Yoo <yohanyoo98@gmail.com>
2023-04-18 21:04:58 +0900

    front-end added

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,42 +1,43 @@
 const asyncHandler = require('express-async-handler')
+const Goal = require('../models/goalModel')
 const getGoals = asyncHandler(async (req, res) => {
-    res.status(200).json({
-        message: 'Get Goals'
-    })
+    const goal = await Goal.find()
+    res.status(200).json(goal)
 })
-const setGoals = (req, res) => {
-    if(!req.body.text){
-        res.status(400)
-        throw new Error('Nothing to Post - no text body')
-    }
-        console.log(req.body);
-        res.status(200).json({
-            message: 'Set Goals'
+const setGoals = asyncHandler(
+    async (req, res) => {
+        if(!req.body.text){
+            res.status(400)
+            throw new Error('Please add a text field')
+        }
+        const goal = await Goal.create({
+            text: req.body.text
         })
+        res.status(200).json(goal)
     }
+) 
 
 const updateGoals = asyncHandler(
     async (req, res) => {
-        if(!req.params.id){
+        const goal = await Goal.findById(req.params.id)
+        if(!goal){
             res.status(400)
-            throw new Error('Please enter a valid id')
+            throw new Error('Goal ID not found')
         }
-        console.log(req.params.id);
-        res.status(200).json({
-            message: `update Goals ${req.params.id}`
-        })
+        const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, {new: true})
+        res.status(200).json(updatedGoal)
     }
 )
 const deleteGoals = asyncHandler(
     async (req, res) => {
-        if(!req.params.id){
+        const goal = await Goal.findById(req.params.id)
+        if(!goal){
             res.status(400)
-            throw new Error('Please enter a valid id')
+            throw new Error('Goal ID not found')
         }
-        console.log(req.params.id);
-        res.status(200).json({
-            message: `Delete  Goals ${req.params.id}`
-        })
+        await Goal.findByIdAndRemove(req.params.id)
+        const list  = await Goal.find()
+       res.status(200).json(list)
     }
 )
 module.exports = {
--- backend/server.js
+++ backend/server.js
@@ -13,6 +13,6 @@ app.use(express.urlencoded({extended: false}))
 app.use('/api/goals', require('./routes/goalRoutes'))
 
 
-app.listen(port, () => console.log(`Server started on port ${port}`))
+app.listen(port, () => console.log(`Server started on port ${port}`.magenta.bold))
 
 
